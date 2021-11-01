e4cc5ec2fe1404a112a6c3ae41528a9537d8a39d
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2021-11-01 14:20:55 -0500

    Imported prompt module into the atm.js file

--- atm.js
+++ atm.js
@@ -2,6 +2,7 @@
 //TODO: Import necessary values from account.js
 const {pin} = require("./account");
 const {balance} = require("./account");
+const prompt = require("prompt-sync")();
 
 function getBalance() {
   //TODO: Return the customer's acct. balance
@@ -34,3 +35,6 @@ function validatePin(enteredPin) {
 
 // let bankBalance = balance;
 // console.log(`This is my bank account balance: ${bankBalance}`);
+
+// let userInput = prompt("What is your name? ")
+// console.log(userInput);
